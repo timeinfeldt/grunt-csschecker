8bdccf21089600d49883ed6258b41fe5e89c73cd
Tim Einfeldt <tim.einfeldt@gmail.com>
2014-01-29 18:01:06 +0100

    code cleanup and config updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,24 +12,6 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        jshint : {
-            all : [
-                'Gruntfile.js',
-                'tasks/*.js',
-                '<%= nodeunit.tests %>'
-            ],
-            options : {
-                jshintrc : '.jshintrc'
-            }
-        },
-
-        // Before generating any new files, remove any previously-created files.
-        clean : {
-            tests : ['tmp']
-        },
-
-        // Configuration to be run (and then tested).
-
         csschecker : {
             dev : {
                 cssSrc : [
@@ -55,17 +37,21 @@ module.exports = function (grunt) {
                             options : {
                                 minUsage : 2
                             }
-                        },
-                        missingCheck : {
-                            options : {
-                                minUsage : 2
-                            }
                         }
                     }
                 }
             }
         },
-
+        jshint : {
+            all : [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options : {
+                jshintrc : '.jshintrc'
+            }
+        },
         // Unit tests.
         nodeunit : {
             tests : ['test/*_test.js']
@@ -77,12 +63,11 @@ module.exports = function (grunt) {
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'csschecker', 'nodeunit']);
+    grunt.registerTask('test', ['csschecker', 'nodeunit']);
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['jshint', 'csschecker:dev']);
