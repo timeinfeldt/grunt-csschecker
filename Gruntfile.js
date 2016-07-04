b270211a9af20593253f198c80bcbde3b63916e1
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-07-04 10:57:09 +0200

    Update code to use es2015 and jshint with eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,44 @@
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function gruntConfig(grunt) {
+  grunt.option('stack', true);
 
-    grunt.option('stack', true);
-
-    grunt.initConfig({
-        csschecker : {
-            dev : {
-                options : {
-                    checkstyle : 'out/checkstyle.xml'
-                },
-                cssSrc : [
-                    'test/**/*.css'
-                ],
-                codeSrc : [
-                    'test/**/*.html'
-                ],
-                checks : {
-                    selectorLengthCheck : {
-                        maxLength : 4
-                    },
-                    classUsageCheck : {
-                        reportAll : false,
-                        minUsage : 3
-                    },
-                    classNoUsageCheck : {
-                    },
-                    declarationsDefinitionsCheck : {
-                        whiteList : [
-                            'background',
-                            'color'
-                        ]
-                    }
-                }
-            }
+  grunt.initConfig({
+    csschecker: {
+      dev: {
+        options: {
+          checkstyle: 'out/checkstyle.xml',
         },
-        jshint : {
-            all : [
-                'Gruntfile.js',
-                'tasks/*.js'
+        cssSrc: [
+          'test/**/*.css',
+        ],
+        codeSrc: [
+          'test/**/*.html',
+        ],
+        checks: {
+          selectorLengthCheck: {
+            maxLength: 4,
+          },
+          classUsageCheck: {
+            reportAll: false,
+            minUsage: 3,
+          },
+          classNoUsageCheck: {
+          },
+          declarationsDefinitionsCheck: {
+            whiteList: [
+              'background',
+              'color',
             ],
-            options : {
-                jshintrc : '.jshintrc'
-            }
-        }
-    });
+          },
+        },
+      },
+    },
+  });
 
     // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks');
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-
-    grunt.registerTask('test', ['csschecker:dev']);
-    grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('test', ['csschecker:dev']);
+  grunt.registerTask('default', ['test']);
 };
--- tasks/csschecker.js
+++ tasks/csschecker.js
@@ -1,186 +1,161 @@
-/*
- * grunt-csschecker
- * https://github.com/timeinfeldt/grunt-csschecker
- *
- * Copyright (c) 2014-2016 Tim Einfeldt
- * Licensed under the MIT license.
- */
-/* global grunt, Promise */
 'use strict';
-var cssCheckerParse = require('../lib/parsers/csschecker'),
-    codeCheckerParse = require('../lib/parsers/codechecker'),
-    Queue = require('promise-queue'),
-    glob = require('glob-promise'),
-    flatten = require('flatten'),
-    checks = require('../lib/checks/checks.js'),
-    reporters = require('../lib/reporters'),
-    sourceMap = require('source-map'),
-    fs = require('fs'),
-    path = require('path');
+/* global grunt, Promise */
+
+const cssCheckerParse = require('../lib/parsers/csschecker');
+const codeCheckerParse = require('../lib/parsers/codechecker');
+const Queue = require('promise-queue');
+const glob = require('glob-promise');
+const flatten = require('flatten');
+const checks = require('../lib/checks/checks.js');
+const reporters = require('../lib/reporters');
+const sourceMap = require('source-map');
+const fs = require('fs');
+const path = require('path');
 
 function reduce(promises, fn, initialValue) {
-    var acc = initialValue;
-    promises.forEach(function (promise) {
-        promise.then(function (value) {
-            acc = fn(acc, value);
-        });
+  let acc = initialValue;
+  promises.forEach(promise => {
+    promise.then(value => {
+      acc = fn(acc, value);
     });
-    return Promise
-        .all(promises)
-        .then(function () {
-            return acc;
-        });
+  });
+
+  return Promise.all(promises).then(() => acc);
 }
 
 function getCssResultsClassNames(results) {
-    return results
-        .filter(function (result) {
-            return result.type === 'class';
-        })
-        .map(function (result) {
-            return result.className;
-        });
+  return results
+    .filter(result => result.type === 'class')
+    .map(cls => cls.className);
 }
 
 function getFilesFromPath(patterns) {
-    if (!patterns) {
-        grunt.fail.warn('No source file paths found.');
-    }
+  if (!patterns) {
+    grunt.fail.warn('No source file paths found.');
+  }
 
-    return Promise.all(patterns.map(function (pattern) {
-        return glob(pattern);
-    }));
+  return Promise.all(patterns.map(pattern => glob(pattern)));
 }
 
-var uniqueArray = function (arr) {
-    return arr.filter(function(elem, pos) {
-        return arr.indexOf(elem) === pos;
-    });
-};
+function uniqueArray(arr) {
+  return arr.filter((elem, pos) => arr.indexOf(elem) === pos);
+}
 
 function objectCombine(keys, values) {
-    var result = {};
-    for (var i = 0; i < keys.length; i++) {
-        result[keys[i]] = values[i];
-    }
-    return result;
+  const result = {};
+  for (let i = 0; i < keys.length; i++) {
+    result[keys[i]] = values[i];
+  }
+  return result;
 }
 
-var mergeClassCounts = function (classCountsList) {
-    var merged = {};
-    classCountsList.forEach(function (classCounts) {
-        Object.keys(classCounts).forEach(function (key) {
-            merged[key] = merged[key] || 0;
-            merged[key] += classCounts[key];
-        });
+function mergeClassCounts(classCountsList) {
+  const merged = {};
+  classCountsList.forEach(classCounts => {
+    Object.keys(classCounts).forEach(key => {
+      merged[key] = merged[key] || 0;
+      merged[key] += classCounts[key];
     });
-    return merged;
-};
+  });
+
+  return merged;
+}
 
 function loadSourceMap(file) {
-    return new Promise(function(resolve, reject) { 
-        fs.readFile(file + '.map', 'utf8', (err, content) => { err ? reject(err) : resolve(content); }); 
-    })
-    .then(function (contents) {
-        var rawSourceMap = JSON.parse(contents);
-        rawSourceMap.sourceRoot = path.dirname(file);
-        return new sourceMap.SourceMapConsumer(rawSourceMap);
-    }, function (err) {
-        if (err.code === 'ENOENT') {
-            return null;
-        }
-        return Promise.reject(err);
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${file}.map`, 'utf8', (err, content) => {
+      if (err) reject(err);
+      else resolve(content);
     });
+  })
+  .then(contents => {
+    const rawSourceMap = JSON.parse(contents);
+    rawSourceMap.sourceRoot = path.dirname(file);
+    return new sourceMap.SourceMapConsumer(rawSourceMap);
+  })
+  .catch(err => {
+    if (err.code === 'ENOENT') {
+      return null;
+    }
+    return Promise.reject(err);
+  });
 }
 
 function mapQueued(numConcurrent, keys, fn) {
-    var queue = new Queue(numConcurrent, Infinity);
-    var promises = keys.map(function (key) {
-        return queue.add(function () {
-            return fn(key);
-        });
-    });
-    return promises;
+  const queue = new Queue(numConcurrent, Infinity);
+
+  return keys.map(key => queue.add(() => fn(key)));
 }
 
 function resolveSourceMaps(results) {
-    var cssFiles = results
-        .map(function (result) { return result.file; })
-        .filter(function (file) { return file.match(/\.css$/); });
-    cssFiles = uniqueArray(cssFiles);
+  let cssFiles = results
+        .map(result => result.file)
+        .filter(file => file.match(/\.css$/));
+  cssFiles = uniqueArray(cssFiles);
 
-    var promises = mapQueued(50, cssFiles, loadSourceMap);
+  const promises = mapQueued(50, cssFiles, loadSourceMap);
 
-    return Promise.all(promises)
-        .then(function (sourceMaps) {
-            var sourceMapsByCssFile = objectCombine(cssFiles, sourceMaps);
+  return Promise.all(promises)
+    .then(sourceMaps => {
+      const sourceMapsByCssFile = objectCombine(cssFiles, sourceMaps);
 
-            return results.map(function (result) {
-                var consumer = sourceMapsByCssFile[result.file];
+      return results.map(result => {
+        const consumer = sourceMapsByCssFile[result.file];
 
-                if (!consumer) {
-                    return result;
-                }
+        if (!consumer) return result;
 
-                var position = consumer.originalPositionFor({
-                    line: result.line,
-                    column: result.column
-                });
-
-                result.file = position.source;
-                result.line = position.line;
-                result.column = position.column;
-
-                return result;
-            });
+        const position = consumer.originalPositionFor({
+          line: result.line,
+          column: result.column,
         });
+
+        // eslint-disable-next-line no-param-reassign
+        result.file = position.source;
+
+        // eslint-disable-next-line no-param-reassign
+        result.line = position.line;
+
+        // eslint-disable-next-line no-param-reassign
+        result.column = position.column;
+
+        return result;
+      });
+    });
 }
 
-module.exports = function (grunt) {
-    grunt.registerMultiTask('csschecker', 'Checks your CSS', function () {
-        var done = this.async(),
-            checksConfig = this.data.checks,
-            self = this;
+module.exports = grunt => {
+  grunt.registerMultiTask('csschecker', 'Checks your CSS', function csschecker() {
+    const done = this.async();
+    const checksConfig = this.data.checks;
 
-        var cssResults = getFilesFromPath(this.data.cssSrc)
-            .then(flatten)
-            .then(function (files) {
-                return Promise.all(files.map(cssCheckerParse));
-            })
-            .then(flatten);
+    getFilesFromPath(this.data.cssSrc)
+      .then(flatten)
+      .then(files => Promise.all(files.map(cssCheckerParse)))
+      .then(flatten)
+      .then(files => Promise.resolve(getCssResultsClassNames(files))
+        .then(classNames => getFilesFromPath(this.data.codeSrc)
+          .then(flatten)
+          .then(classFiles => {
+            const promises = mapQueued(50, classFiles, file => codeCheckerParse(file, classNames));
+            return reduce(promises, (acc, value) => mergeClassCounts([acc, value]), {});
+          })
+          .catch(err => { throw err; })
+        )
+        .then(
+          classCounts =>
+            Object.keys(checksConfig)
+              .map(checkName => checks[checkName](files, classCounts, checksConfig[checkName]))
+        )
+        .then(flatten)
+        .then(results => {
+          resolveSourceMaps(results).then(innerResults => {
+            if (this.data.options.checkstyle) {
+              grunt.file.write(this.data.options.checkstyle, reporters.checkstyle(innerResults));
+            }
 
-        cssResults.then(function (results) {
-            return Promise.resolve(getCssResultsClassNames(results))
-                .then(function (classNames) {
-                    return getFilesFromPath(self.data.codeSrc)
-                        .then(flatten)
-                        .then(function (files) {
-                            var promises = mapQueued(50, files, function (file) {
-                                return codeCheckerParse(file, classNames);
-                            });
-                            return reduce(promises, function(acc, value) {
-                                return mergeClassCounts([acc, value]);
-                            }, {});
-                        });
-                })
-                .then(function (classCounts) {
-                    return Object.keys(checksConfig).map(function (checkName) {
-                        var check = checks[checkName],
-                            config = checksConfig[checkName];
-
-                        return check(results, classCounts, config);
-                    });
-                })
-                .then(flatten)
-                .then(function (results) {
-                    resolveSourceMaps(results).then(function (innerResults) {
-                        if (self.data.options.checkstyle) {
-                            grunt.file.write(self.data.options.checkstyle, reporters.checkstyle(results));
-                        }
-
-                        done();
-                    });
-                });
-        });
-    });
+            done();
+          });
+        })
+      );
+  });
 };
